6a50ae53ce32f4a0c03e3eb3a27a08366027f004
Orion Palmer <opalmer1989@gmail.com>
2022-12-30 14:52:48 -0500

    Update ConnectionHelper.cs

--- Helpers/ConnectionHelper.cs
+++ Helpers/ConnectionHelper.cs
@@ -19,7 +19,7 @@ namespace ContactPro.Helpers
         private static string BuildConnectionString(string databaseUrl)
         {
             var databaseUri = new Uri(databaseUrl);
-            var userInfo = databaseUri.UserInfo.Split(';');
+            var userInfo = databaseUri.UserInfo.Split(':');
             var builder = new NpgsqlConnectionStringBuilder
             {
                 Host = databaseUri.Host,
